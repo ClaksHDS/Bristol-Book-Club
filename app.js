bdc50a6a963b763455f956140e480157addbf351
[claks HDS] <you@example.com>
2023-01-03 16:35:13 +0000

    updates + add JS file

---
+++ app.js
@@ -0,0 +1,39 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
+
+const btn = document.querySelector(".join-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+const print = function (e) {
+  console.log("Hi Bob");
+};
+
+btn.addEventListener("click", print);
+prevBtn.addEventListener("click", print);
